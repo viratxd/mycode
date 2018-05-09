71e7961c61062ee0fb2e2cf2bfa9e2ac9dd23815
viratxd <38873038+viratxd@users.noreply.github.com>
2018-05-09 11:26:43 +0000

    Update share.js

--- share.js
+++ share.js
@@ -1,6 +1,6 @@
 function getNumber() {
-    var minNumber = 10; 
-    var maxNumber = 100; 
+    var minNumber = 50; 
+    var maxNumber = 200; 
     var randomnumber = Math.floor(Math.random() * (maxNumber + 1) + minNumber); 
     document.getElementById("share").innerHTML = randomnumber; 
     return false; 
